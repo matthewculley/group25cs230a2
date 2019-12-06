5824f065b70f375a02e18e57a30e504ae7824c92
Ejones4321 <57711255+Ejones4321@users.noreply.github.com>
2019-12-06 15:16:45 +0000

    Fully Functional smart enemies

--- group25cs230a2/group25cs230a2/src/enemies/EnemySmart.java
+++ group25cs230a2/group25cs230a2/src/enemies/EnemySmart.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import game.Map;
 
 public class EnemySmart extends Enemy {
-	public EnemySmart(int x, int y) {
+	private Map map;
+	public EnemySmart(int x, int y,Map map) {
 		super(x, y);
+		this.map = map;
 		super.setSprite("enemySmart.png");
 	}
 	private CoordinateCost coordinateMatch(ArrayList<CoordinateCost> a) {
@@ -17,6 +19,14 @@ public class EnemySmart extends Enemy {
 		}
 		return null;
 	}
+	private boolean coordinateMatch(ArrayList<CoordinateCost> a,int x,int y) {
+		for(int i = 0; i<a.size();i++) {
+			if(a.get(i).x == x && a.get(i).y == y ) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	private boolean currentMatch(CoordinateCost a) {
 		if(a.x==getX()&&a.y==getY()) {
@@ -25,66 +35,93 @@ public class EnemySmart extends Enemy {
 		return false;
 	}
 	
+	
 	public void move (Map map) {
 		int px = map.getPlayer().getX();
 		int py = map.getPlayer().getY();
-		
+		System.out.print("\nPLAYER X + Y" +px+"  "+py);
 		
 		ArrayList<CoordinateCost> openList = new ArrayList<>();
 		ArrayList<CoordinateCost> closedList = new ArrayList<>();
 		openList.add(new CoordinateCost(getX(),getY(),0,null,px,py,map));
 		
-		while(coordinateMatch(closedList) != null || openList.size()==0) {
-			CoordinateCost current = openList.get(0);
-			for(int i = 0;i < openList.size();i++) {
-				if(openList.get(i).totalCost<current.totalCost) {
-					current = openList.get(i);
-				}
-				openList.remove(current);
-				closedList.add(current);
-				
-				CoordinateCost above = new CoordinateCost(current.x,current.y-1,current.distanceTravelled+1,current,px,py,map);
-				CoordinateCost below = new CoordinateCost(current.x,current.y+1,current.distanceTravelled+1,current,px,py,map);
-				CoordinateCost left = new CoordinateCost(current.x-1,current.y,current.distanceTravelled+1,current,px,py,map);
-				CoordinateCost right = new CoordinateCost(current.x+1,current.y,current.distanceTravelled+1,current,px,py,map);
-				
-				if(checkValidMove(map, above.x, above.y)&&!closedList.contains(above)) {
-					if(!openList.contains(above)) {
-						openList.add(above);
-					}else if(above.distanceTravelled>current.distanceTravelled+1) {
-						above.parent = current;
-						above.distanceTravelled = current.distanceTravelled+1;
+			while((coordinateMatch(closedList) == null) && openList.size()>0) {
+				CoordinateCost current = openList.get(0);
+				for(int i = 1;i < openList.size();i++) {
+					if(openList.get(i).totalCost<current.totalCost) {
+						current = openList.get(i);
 					}
 				}
-				if(checkValidMove(map, below.x, below.y)&&!closedList.contains(below)) {
-					if(!openList.contains(below)) {
-						openList.add(below);
+					int o = openList.size();
+					openList.remove(current);
+					if(o-openList.size() == 1) {
+						System.out.println(" \n REMOVED 1");
+					}
+					closedList.add(current);
+					
+					System.out.print("\n OPENLIST SIZE"+openList.size()+" \n CLOSED LIST SIZE: " + closedList.size() +"\nFILLING LIST");
+					
+					CoordinateCost above = new CoordinateCost(current.x,current.y-1,current.distanceTravelled+1,current,px,py,map);
+					CoordinateCost below = new CoordinateCost(current.x,current.y+1,current.distanceTravelled+1,current,px,py,map);
+					CoordinateCost left = new CoordinateCost(current.x-1,current.y,current.distanceTravelled+1,current,px,py,map);
+					CoordinateCost right = new CoordinateCost(current.x+1,current.y,current.distanceTravelled+1,current,px,py,map);
+					
+					if(checkValidMove(map, above.x, above.y)&&!(coordinateMatch(closedList,above.x,above.y))) {
+						if(!(coordinateMatch(openList,above.x,above.y))) {
+							openList.add(above);
+							System.out.println(" \n ABOVE X ABD Y" + above.x+" "+above.y);
+						}else if(above.distanceTravelled>current.distanceTravelled+1) {
+							above.parent = current;
+							above.distanceTravelled = current.distanceTravelled+1;
+						}
+					}
+					if(checkValidMove(map, below.x, below.y)&&!(coordinateMatch(closedList,below.x,below.y))) {
+						if(!(coordinateMatch(openList,below.x,below.y))) {
+							openList.add(below);
+						}else if(below.distanceTravelled>current.distanceTravelled+1) {
+							below.parent = current;
+							below.distanceTravelled = current.distanceTravelled+1;
+						}
+					}
+					if(checkValidMove(map, left.x, left.y)&&!(coordinateMatch(closedList,left.x,left.y))) {
+						if(!(coordinateMatch(openList,left.x,left.y))) {
+							openList.add(left);
+						}else if(left.distanceTravelled>current.distanceTravelled+1) {
+							left.parent = current;
+							left.distanceTravelled = current.distanceTravelled+1;
+						}
+					}
+					if(checkValidMove(map, right.x, right.y)&&!(coordinateMatch(closedList,right.x,right.y))) {
+						if(!(coordinateMatch(openList,right.x,right.y))) {
+							openList.add(right);
+						}else if(right.distanceTravelled>current.distanceTravelled+1) {
+							right.parent = current;
+							right.distanceTravelled = current.distanceTravelled+1;
+						}
 					}
 				}
-				if(checkValidMove(map, left.x, left.y)&&!closedList.contains(left)) {
-					if(!openList.contains(left)) {
-						openList.add(left);
-					}
-				}
-				if(checkValidMove(map, right.x, right.y)&&!closedList.contains(right)) {
-					if(!openList.contains(right)) {
-						openList.add(right);
-					}
-				}
-			}
-		}
+			
 		CoordinateCost foundCoordinate = coordinateMatch(closedList);
 		ArrayList<CoordinateCost> path = new ArrayList<>();	
+		
+		// System.out.println(" \n FOUND COORDINATE : " +foundCoordinate.x +"  "+ foundCoordinate.y);
+		
 		if(foundCoordinate != null) {
-			path.set(path.size(),foundCoordinate);
+			path.add(0,foundCoordinate);
 			CoordinateCost currentPath = foundCoordinate;
 			do{
-				path.set(path.size(), currentPath);
-				currentPath = currentPath.parent;
+				path.add(0, currentPath);
+				if(currentPath.parent!= null) {
+					currentPath = currentPath.parent;
+				}
 			}while(!currentMatch(currentPath));
+		}else {
+			System.out.println(" \n FAILED FINDING PATH");
 		}
 		if(path.size()>0) {
 			this.setPosition(path.get(0).x,path.get(0).y);
+		}else {
+			System.out.println(" \n FAILED creating path");
 		}
 	}
 	@Override
