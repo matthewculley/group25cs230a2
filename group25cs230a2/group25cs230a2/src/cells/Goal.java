e34416c5cc6408fcba7516f914d07cdee06e91fd
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-07 18:17:43 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/cells/Goal.java
+++ group25cs230a2/group25cs230a2/src/cells/Goal.java
@@ -1,31 +1,33 @@
 package cells;
 
+/**
+ * A class that defines Goal
+ * @author Group25
+ */
+
 public class Goal extends Cell{
 	
 	public Goal() {
 		super("goal.png", true);
 	}
 	
+	/**
+         * Overwrites the method isGoal
+         * @return true
+         */
+	
 	@Override
 	public boolean isGoal() {
 		return true;
 	}
 
+	/**
+  	 * Overwrites the method getCellName
+	 * @return "goal"
+ 	 */
+	
 	@Override
 	public String getCellName() {
-		// TODO Auto-generated method stub
 		return "goal";
 	}
-
-	
-//	public void checkIfTriggered() {
-//		if (trigger == 0) {
-//			trigger = 1;
-//			endLevel();
-//		}
-//	}
-//	
-//	private void endLevel() {
-//		lvl.nextLevel();
-//	}
 }
