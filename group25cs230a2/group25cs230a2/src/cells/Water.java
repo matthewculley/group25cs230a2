e3419998134d839418e8520637b35ad7fb33550d
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-07 19:14:09 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/cells/Water.java
+++ group25cs230a2/group25cs230a2/src/cells/Water.java
@@ -3,11 +3,23 @@ package cells;
 import collectibles.*;
 import player.*;
 
+/**
+ * A class that defines Water.
+ * @author Group25
+ * @version 1.1
+ */
 
 public class Water extends Cell{
+	
 	public Water () {
 		super("water.png", true);
 	}	
+	
+	/**
+ 	 * A method that checks if the player have Flippers in their inventory.
+ 	 * @param p The player
+	 * @return True/False
+ 	 */
 	
 	public boolean isPassable(Player p) {
 		if (p.getInventory().hasItem(new Flippers())) {
@@ -17,6 +29,11 @@ public class Water extends Cell{
 		}
 	}
 
+	/**
+ 	 * Overrides the method getCellName.
+ 	 * @return The cell name: water
+ 	 */
+	
 	@Override
 	public String getCellName() {
 		return "water";
