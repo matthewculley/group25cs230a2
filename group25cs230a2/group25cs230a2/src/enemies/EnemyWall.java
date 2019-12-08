f32bba636af3fced099860e39d4a7661d74c22ea
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 17:56:27 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/enemies/EnemyWall.java
+++ group25cs230a2/group25cs230a2/src/enemies/EnemyWall.java
@@ -2,12 +2,26 @@ package enemies;
 
 import game.*;
 
+/**
+ * A subclass that defines wall enemy.
+ * @author Ethan J
+ * @version 1.3
+ */
+
 public class EnemyWall extends Enemy {
+	
 	private int prevX;
 	private int prevY;
 	private char direction; //of wall
 	private char rotation; // cw or anti cw
 	
+	/**
+	 * Constructs the wall enemy.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param rotation The rotation of the enemy.
+	 * @param direction The direction of the enemy is going.
+ 	 */
 	public EnemyWall(int x, int y, char rotation, char direction) {
 		super(x, y);
 		super.setSprite("enemyWall.png");
@@ -15,66 +29,59 @@ public class EnemyWall extends Enemy {
 		setRotation(rotation);
 	}
 
-	/**
-	 * @return the direction
-	 */
 	private char getDirection() {
 		return direction;
 	}
 
-	/**
-	 * @param direction the direction to set
-	 */
 	private void setDirection(char direction) {
 		this.direction = direction;
 	}
-	/**
-	 * @return the rotation
-	 */
+
 	private char getRotation() {
 		return rotation;
 	}
 
-	/**
-	 * @param rotation the rotation to set
-	 */
 	private void setRotation(char rotation) {
 		this.rotation = rotation;
 	}
 	
+	/**
+ 	 * A method that defines the wall enemy's movement.
+ 	 * @param map The map.
+ 	 */
 	public void move(Map map) {
 		switch (direction) {
 		case 'u':
 			switch (rotation) {
 				case 'c':
-					if(!checkValidMove(map, getX(), getY()-1)||(getX()==prevX&&getY()-1==prevY)){ // wall still above
-						if(checkValidMove(map, getX()-1, getY())) { // can move left
-							this.setPosition(getX()-1, getY());
-						}else if(checkValidMove(map, getX(), getY()+1)) { // cant move left but can down
-							this.setPosition(getX(), getY()+1);
+					if(!checkValidMove(map, getX(), getY() - 1)||(getX() == prevX && getY() - 1 == prevY)){ // wall still above
+						if(checkValidMove(map, getX() - 1, getY())) { // can move left
+							this.setPosition(getX() - 1, getY());
+						}else if(checkValidMove(map, getX(), getY() + 1)) { // cant move left but can down
+							this.setPosition(getX(), getY() + 1);
 							this.setDirection('l');
-						}else if(checkValidMove(map, getX()+1, getY())) { // can only move right
-							this.setPosition(getX()+1, getY());
+						}else if(checkValidMove(map, getX() + 1, getY())) { // can only move right
+							this.setPosition(getX() + 1, getY());
 							this.setDirection('d');
 						}
 					}else { // wall above gone
-						this.setPosition(getX(), getY()-1);
+						this.setPosition(getX(), getY() - 1);
 						this.setDirection('r');
 					}
 					break;
 				case 'a':
-					if(!checkValidMove(map, getX(), getY()-1)||(getX()==prevX&&getY()-1==prevY)){ // wall still above
-						if(checkValidMove(map, getX()+1, getY())) { // can move right
-							this.setPosition(getX()+1, getY());
-						}else if(checkValidMove(map, getX(), getY()+1)) { // cant move right but can down
-							this.setPosition(getX(), getY()+1);
+					if(!checkValidMove(map, getX(), getY() - 1) || (getX() == prevX && getY() - 1 == prevY)){ // wall still above
+						if(checkValidMove(map, getX() + 1, getY())) { // can move right
+							this.setPosition(getX() + 1, getY());
+						}else if(checkValidMove(map, getX(), getY() + 1)) { // cant move right but can down
+							this.setPosition(getX(), getY() + 1);
 							this.setDirection('r');
-						}else if(checkValidMove(map, getX()-1, getY())) { // can only move left
-							this.setPosition(getX()-1, getY());
+						}else if(checkValidMove(map, getX() - 1, getY())) { // can only move left
+							this.setPosition(getX() - 1, getY());
 							this.setDirection('d');
 						}
 					}else { //wall above gone
-						this.setPosition(getX(), getY()-1);
+						this.setPosition(getX(), getY() - 1);
 						this.setDirection('l');
 					}
 					break;
@@ -83,35 +90,35 @@ public class EnemyWall extends Enemy {
 		case 'd':
 			switch (rotation) {
 				case 'c':
-					if(!checkValidMove(map, getX(), getY()+1)||(getX()==prevX&&getY()+1==prevY)){ // wall still below
-						if(checkValidMove(map, getX()+1, getY())) { // can move right
+					if(!checkValidMove(map, getX(), getY() + 1) || (getX() == prevX && getY() + 1 == prevY)){ // wall still below
+						if(checkValidMove(map, getX() + 1, getY())) { // can move right
 							System.out.println("OOOOOO");
-							this.setPosition(getX()+1, getY());
-						}else if(checkValidMove(map, getX(), getY()-1)) { // cant move right but can up
-							this.setPosition(getX(), getY()-1);
+							this.setPosition(getX() + 1, getY());
+						}else if(checkValidMove(map, getX(), getY() - 1)) { // cant move right but can up
+							this.setPosition(getX(), getY() - 1);
 							this.setDirection('r');
-						}else if(checkValidMove(map, getX()-1, getY())) { // can only move left
-							this.setPosition(getX()-1, getY());
+						}else if(checkValidMove(map, getX() - 1, getY())) { // can only move left
+							this.setPosition(getX() - 1, getY());
 							this.setDirection('u');
 						}
 					}else { // wall below gone
-						this.setPosition(getX(), getY()+1);
+						this.setPosition(getX(), getY() + 1);
 						this.setDirection('l');
 					}
 					break;
 				case 'a':
-					if(!checkValidMove(map, getX(), getY()+1)||(getX()==prevX&&getY()+1==prevY)){ // wall still below
-						if(checkValidMove(map, getX()-1, getY())) { // can move left
-							this.setPosition(getX()-1, getY());
-						}else if(checkValidMove(map, getX(), getY()-1)) { // cant move left but can up
-							this.setPosition(getX(), getY()-1);
+					if(!checkValidMove(map, getX(), getY() + 1) || (getX() == prevX && getY() + 1 == prevY)){ // wall still below
+						if(checkValidMove(map, getX() - 1, getY())) { // can move left
+							this.setPosition(getX() - 1, getY());
+						}else if(checkValidMove(map, getX(), getY() - 1)) { // cant move left but can up
+							this.setPosition(getX(), getY() - 1);
 							this.setDirection('l');
-						}else if(checkValidMove(map, getX()-1, getY())) { // can only move right
-							this.setPosition(getX()+1, getY());
+						}else if(checkValidMove(map, getX() - 1, getY())) { // can only move right
+							this.setPosition(getX() + 1, getY());
 							this.setDirection('u');
 						}
 					}else { // wall below gone
-						this.setPosition(getX(), getY()+1);
+						this.setPosition(getX(), getY() + 1);
 						this.setDirection('r');
 					}
 					break;
@@ -120,34 +127,34 @@ public class EnemyWall extends Enemy {
 		case 'l':
 			switch (rotation) {
 				case 'c':
-					if(!checkValidMove(map, getX()-1, getY())||(getX()-1==prevX&&getY()==prevY)){ // wall still left
-						if(checkValidMove(map, getX(), getY()+1)) { // can move down
-							this.setPosition(getX(), getY()+1);
-						}else if(checkValidMove(map, getX()+1, getY())) { // cant move down but can right
-							this.setPosition(getX()+1, getY());
+					if(!checkValidMove(map, getX() - 1, getY()) || (getX() - 1 == prevX && getY() == prevY)){ // wall still left
+						if(checkValidMove(map, getX(), getY() + 1)) { // can move down
+							this.setPosition(getX(), getY() + 1);
+						}else if(checkValidMove(map, getX() + 1, getY())) { // cant move down but can right
+							this.setPosition(getX() + 1, getY());
 							this.setDirection('d');
-						}else if(checkValidMove(map, getX(), getY()-1)) { // can only move up
-							this.setPosition(getX(), getY()-1);
+						}else if(checkValidMove(map, getX(), getY() - 1)) { // can only move up
+							this.setPosition(getX(), getY() - 1);
 							this.setDirection('r');
 						}
 					}else { // wall left gone
-						this.setPosition(getX()-1, getY());
+						this.setPosition(getX() - 1, getY());
 						this.setDirection('u');
 					}
 					break;
 				case 'a':
-					if(!checkValidMove(map, getX()-1, getY())||(getX()-1==prevX&&getY()==prevY)){ // wall still left
-						if(checkValidMove(map, getX(), getY()-1)) { // can move up
-							this.setPosition(getX(), getY()-1);
-						}else if(checkValidMove(map, getX()+1, getY())) { // cant move up but can right
-							this.setPosition(getX()+1, getY());
+					if(!checkValidMove(map, getX() - 1, getY()) || (getX() - 1 == prevX && getY() == prevY)){ // wall still left
+						if(checkValidMove(map, getX(), getY() - 1)) { // can move up
+							this.setPosition(getX(), getY() - 1);
+						}else if(checkValidMove(map, getX() + 1, getY())) { // cant move up but can right
+							this.setPosition(getX() + 1, getY());
 							this.setDirection('u');
-						}else if(checkValidMove(map, getX(), getY()+1)) { // can only move down
-							this.setPosition(getX(), getY()+1);
+						}else if(checkValidMove(map, getX(), getY() + 1)) { // can only move down
+							this.setPosition(getX(), getY() + 1);
 							this.setDirection('r');
 						}
 					}else { // wall left gone
-						this.setPosition(getX()-1, getY());
+						this.setPosition(getX() - 1, getY());
 						this.setDirection('d');
 					}
 					break;					
@@ -156,44 +163,48 @@ public class EnemyWall extends Enemy {
 		case 'r':
 			switch (rotation) {
 				case 'c':
-					if(!checkValidMove(map, getX()+1, getY())||(getX()+1==prevX&&getY()==prevY)){ // wall still right
-						if(checkValidMove(map, getX(), getY()-1)) { // can move up
-							this.setPosition(getX(), getY()-1);
-						}else if(checkValidMove(map, getX()-1, getY())) { // cant move up but can left
-							this.setPosition(getX()-1, getY());
+					if(!checkValidMove(map, getX() + 1, getY()) || (getX() + 1 == prevX && getY() == prevY)){ // wall still right
+						if(checkValidMove(map, getX(), getY() - 1)) { // can move up
+							this.setPosition(getX(), getY() - 1);
+						}else if(checkValidMove(map, getX() - 1, getY())) { // cant move up but can left
+							this.setPosition(getX() - 1, getY());
 							this.setDirection('u');
-						}else if(checkValidMove(map, getX(), getY()+1)) { // can only move down
-							this.setPosition(getX(), getY()+1);
+						}else if(checkValidMove(map, getX(), getY() + 1)) { // can only move down
+							this.setPosition(getX(), getY() + 1);
 							this.setDirection('l');
 						}
 					}else { // wall right gone
-						this.setPosition(getX()+1, getY());
+						this.setPosition(getX() + 1, getY());
 						this.setDirection('d');
 					}
 					break;
 				case 'a':
-					if(!checkValidMove(map, getX()+1, getY())||(getX()+1==prevX&&getY()==prevY)){ // wall still right
-						if(checkValidMove(map, getX(), getY()+1)) { // can move down
-							this.setPosition(getX(), getY()+1);
-						}else if(checkValidMove(map, getX()-1, getY())) { // cant move down but can left
-							this.setPosition(getX()-1, getY());
+					if(!checkValidMove(map, getX() + 1, getY()) || (getX() + 1 == prevX && getY() == prevY)){ // wall still right
+						if(checkValidMove(map, getX(), getY() + 1)) { // can move down
+							this.setPosition(getX(), getY() + 1);
+						}else if(checkValidMove(map, getX() - 1, getY())) { // cant move down but can left
+							this.setPosition(getX() - 1, getY());
 							this.setDirection('d');
-						}else if(checkValidMove(map, getX(), getY()-1)) { // can only move up
-							this.setPosition(getX(), getY()-1);
+						}else if(checkValidMove(map, getX(), getY() - 1)) { // can only move up
+							this.setPosition(getX(), getY() - 1);
 							this.setDirection('l');
 						}
 					}else { // wall right gone
-						this.setPosition(getX()+1, getY());
+						this.setPosition(getX() + 1, getY());
 						this.setDirection('u');
 					}
 					break;
 			}
 		}
-//		System.out.println("direction of wall " + direction +" rotation: " + rotation);
+		//System.out.println("direction of wall " + direction +" rotation: " + rotation);
 		prevX = getX();
 		prevY = getY();
 	}
 	
+	/**
+ 	 * Overrides the method getEnemyName.
+ 	 * @return The rotation and direction of the wall enemy.
+ 	 */
 	@Override
 	public String getEnemyName() {
 		return "enemy:wall:" + getRotation() + ":" + getDirection();
