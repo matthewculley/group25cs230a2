3be2f51f1a7d8ececf1afb1acf0a94d3d27e2cd6
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 19:00:36 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/collectibles/Flippers.java
+++ group25cs230a2/group25cs230a2/src/collectibles/Flippers.java
@@ -1,16 +1,34 @@
 package collectibles;
 
+/**
+ * A subclass that defines Flippers.
+ * @author Evans
+ * @version 1.1
+ */
+
 public class Flippers extends Collectible {
 
+	/**
+ 	 * Constructor method that spawns the flippers in the origin (0,0).
+ 	 * @author 
+	 */
 	public Flippers() {
 		super(0, 0, "flippers.png");
 	}
+	
+	/**
+ 	 * Spawns in the specific coordinate.
+ 	 */
 	public Flippers(int x, int y) {
 		super(x, y, "flippers.png");
 	}
+	
+	/**
+ 	 * Get the collectible name.
+ 	 * @return The collectible name: flippers.
+ 	 */
 	@Override
 	public String getCollectibleName() {
 		return "flippers";
 	}
-	
 }
