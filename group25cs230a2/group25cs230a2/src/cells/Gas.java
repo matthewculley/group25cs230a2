cc99f25a05b792315c8200404f96598fecd453b8
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 06:40:55 +0000

    JavaDoc comments (Private method for set?)

--- group25cs230a2/group25cs230a2/src/cells/Gas.java
+++ group25cs230a2/group25cs230a2/src/cells/Gas.java
@@ -1,29 +1,44 @@
 package cells;
 
-
 import java.util.Random;
 import game.Map;
 
-public class Gas extends Cell{
+/**
+ * A class that defines Gas.
+ * @author Group25
+ * @version 1.0
+ */
 
+public class Gas extends Cell {
 
 	private int x;
 	private int y;
 	
+	/**
+ 	 * Constructs Gas in the specific coordinate and set the image name and passable.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+ 	 */
+	
 	public Gas(int x,int y) {
 		super("gas.png", true);
-
 		setX(x);
 		setY(y);
 	}
 	
-
+	/**
+ 	 * Constructs Gas without coordinate.
+ 	 */
 	
 	public Gas() {
 		super("gas.png", true);
 	}
 
-
+	/**
+ 	 * A method that makes the gas spread.
+ 	 * @param map The map
+ 	 */
+	
 	public void infect(Map map) {
 		Gas newGas;
 		//add gas right
@@ -49,11 +64,9 @@ public class Gas extends Cell{
 			map.addGas(newGas);
 
 		}
-	
-//	   System.out.println("returning null");
+	//System.out.println("returning null");
 	}
 	
-
 	private void setX(int x) {
 		this.x = x;		
 	}
@@ -62,17 +75,31 @@ public class Gas extends Cell{
 		this.y = y;		
 	}
 
+	/**
+ 	 * Get the x coordinate.
+ 	 * @return The x coordinate.
+ 	 */
+	
 	public int getX() {
 		return x;
 	}
 
+	/**
+ 	 * Get the y coordinate.
+ 	 * @return The y coordinate.
+ 	 */
+	
 	public int getY() {
 		return y;
 	}
 
+	/**
+ 	 * Overrides the method getCellName.
+ 	 * @return The cell name: gas.
+ 	 */
+	
 	@Override
 	public String getCellName() {
 		return "gas";
 	}
-	
 }
