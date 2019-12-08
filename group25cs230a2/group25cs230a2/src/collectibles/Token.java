86bbfc0b76aa169556548dc76fb92757ff8565e6
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 19:14:01 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/collectibles/Token.java
+++ group25cs230a2/group25cs230a2/src/collectibles/Token.java
@@ -1,22 +1,38 @@
 package collectibles;
 
+/**
+ * A subclass that defines Token.
+ * @author Evans, Matt
+ * @version 1.2
+ */
+
 public class Token extends Collectible {
 
-	 protected int numTokens;
+	protected int numTokens;
 
+	/**
+	 * Constructor method that spawns the token in specific coordinate.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+ 	 */
 	public Token(int x, int y) { // dif constructor needed.
 		super(x, y, "token.png");
 
 	}
 
+	/**
+ 	 * Update the image.
+ 	 */
 	public Token() {
 		super("token.png");
 	}
 
+	/**
+ 	 * Overrides the method getCollectibleName.
+ 	 * @return The collectible name: token.
+ 	 */
 	@Override
 	public String getCollectibleName() {
 		return "token";
 	}
-
-
 }
