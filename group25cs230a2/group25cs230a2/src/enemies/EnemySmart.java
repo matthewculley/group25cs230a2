67b3c9b9a07e2414fbc3eec31c950a8a8eb88dfb
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 17:32:39 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/enemies/EnemySmart.java
+++ group25cs230a2/group25cs230a2/src/enemies/EnemySmart.java
@@ -5,23 +5,39 @@ import java.util.Random;
 
 import game.Map;
 
+/**
+ * A subclass that defines smart enemy.
+ * @author Jack
+ * @version 1.3
+ */
+
 public class EnemySmart extends Enemy {
+	
 	private Map map;
+	
+	/**
+ 	 * Constructs the smart enemy in the specific coordinate.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param map The map.
+ 	 */
 	public EnemySmart(int x, int y,Map map) {
 		super(x, y);
 		this.map = map;
 		super.setSprite("enemySmart.png");
 	}
+	
 	private CoordinateCost coordinateMatch(ArrayList<CoordinateCost> a) {
-		for(int i = 0; i<a.size();i++) {
+		for(int i = 0; i < a.size(); i++) {
 			if(a.get(i).isPlayer()) {
 				return a.get(i);
 			}
 		}
 		return null;
 	}
+	
 	private boolean coordinateMatch(ArrayList<CoordinateCost> a,int x,int y) {
-		for(int i = 0; i<a.size();i++) {
+		for(int i = 0; i < a.size(); i++) {
 			if(a.get(i).x == x && a.get(i).y == y ) {
 				return true;
 			}
@@ -30,74 +46,77 @@ public class EnemySmart extends Enemy {
 	}
 	
 	private boolean currentMatch(CoordinateCost a) {
-		if(a.x==getX()&&a.y==getY()) {
+		if(a.x == getX() && a.y == getY()) {
 			return true;
 		}
 		return false;
 	}
 	
-	
+	/**
+ 	 * A method that defines the movement of the smart enemy.
+ 	 * @param map The map.
+ 	 */
 	public void move (Map map) {
 		int px = map.getPlayer().getX();
 		int py = map.getPlayer().getY();
-//		System.out.print("\nPLAYER X + Y" +px+"  "+py);
+		//System.out.print("\nPLAYER X + Y" +px+"  "+py);
 		
 		ArrayList<CoordinateCost> openList = new ArrayList<>();
 		ArrayList<CoordinateCost> closedList = new ArrayList<>();
-		openList.add(new CoordinateCost(getX(),getY(),0,null,px,py,map));
+		openList.add(new CoordinateCost(getX(), getY(), 0, null, px, py, map));
 		
 			while((coordinateMatch(closedList) == null) && openList.size()>0) {
 				CoordinateCost current = openList.get(0);
-				for(int i = 1;i < openList.size();i++) {
-					if(openList.get(i).totalCost<current.totalCost) {
+				for(int i = 1; i < openList.size(); i++) {
+					if(openList.get(i).totalCost < current.totalCost) {
 						current = openList.get(i);
 					}
 				}
 					int o = openList.size();
 					openList.remove(current);
 					if(o-openList.size() == 1) {
-//						System.out.println(" \n REMOVED 1");
+					//System.out.println(" \n REMOVED 1");
 					}
 					closedList.add(current);
 					
-//					System.out.print("\n OPENLIST SIZE"+openList.size()+" \n CLOSED LIST SIZE: " + closedList.size() +"\nFILLING LIST");
+					//System.out.print("\n OPENLIST SIZE"+openList.size()+" \n CLOSED LIST SIZE: " + closedList.size() +"\nFILLING LIST");
 					
-					CoordinateCost above = new CoordinateCost(current.x,current.y-1,current.distanceTravelled+1,current,px,py,map);
-					CoordinateCost below = new CoordinateCost(current.x,current.y+1,current.distanceTravelled+1,current,px,py,map);
-					CoordinateCost left = new CoordinateCost(current.x-1,current.y,current.distanceTravelled+1,current,px,py,map);
-					CoordinateCost right = new CoordinateCost(current.x+1,current.y,current.distanceTravelled+1,current,px,py,map);
+					CoordinateCost above = new CoordinateCost(current.x, current.y - 1, current.distanceTravelled + 1, current, px, py, map);
+					CoordinateCost below = new CoordinateCost(current.x, current.y + 1, current.distanceTravelled + 1, current, px, py, map);
+					CoordinateCost left = new CoordinateCost(current.x - 1, current.y, current.distanceTravelled + 1, current, px, py, map);
+					CoordinateCost right = new CoordinateCost(current.x + 1, current.y, current.distanceTravelled + 1, current, px, py, map);
 					
-					if(checkValidMove(map, above.x, above.y)&&!(coordinateMatch(closedList,above.x,above.y))) {
-						if(!(coordinateMatch(openList,above.x,above.y))) {
+					if(checkValidMove(map, above.x, above.y) &&! (coordinateMatch(closedList, above.x, above.y))) {
+						if(!(coordinateMatch(openList, above.x, above.y))) {
 							openList.add(above);
-//							System.out.println(" \n ABOVE X ABD Y" + above.x+" "+above.y);
-						}else if(above.distanceTravelled>current.distanceTravelled+1) {
+					//System.out.println(" \n ABOVE X ABD Y" + above.x+" "+above.y);
+						} else if(above.distanceTravelled>current.distanceTravelled + 1) {
 							above.parent = current;
-							above.distanceTravelled = current.distanceTravelled+1;
+							above.distanceTravelled = current.distanceTravelled + 1;
 						}
 					}
-					if(checkValidMove(map, below.x, below.y)&&!(coordinateMatch(closedList,below.x,below.y))) {
-						if(!(coordinateMatch(openList,below.x,below.y))) {
+					if(checkValidMove(map, below.x, below.y) &&! (coordinateMatch(closedList, below.x, below.y))) {
+						if(!(coordinateMatch(openList, below.x, below.y))) {
 							openList.add(below);
-						}else if(below.distanceTravelled>current.distanceTravelled+1) {
+						} else if(below.distanceTravelled > current.distanceTravelled + 1) {
 							below.parent = current;
-							below.distanceTravelled = current.distanceTravelled+1;
+							below.distanceTravelled = current.distanceTravelled + 1;
 						}
 					}
-					if(checkValidMove(map, left.x, left.y)&&!(coordinateMatch(closedList,left.x,left.y))) {
-						if(!(coordinateMatch(openList,left.x,left.y))) {
+					if(checkValidMove(map, left.x, left.y) &&! (coordinateMatch(closedList, left.x, left.y))) {
+						if(!(coordinateMatch(openList, left.x, left.y))) {
 							openList.add(left);
-						}else if(left.distanceTravelled>current.distanceTravelled+1) {
+						} else if(left.distanceTravelled > current.distanceTravelled + 1) {
 							left.parent = current;
-							left.distanceTravelled = current.distanceTravelled+1;
+							left.distanceTravelled = current.distanceTravelled + 1;
 						}
 					}
-					if(checkValidMove(map, right.x, right.y)&&!(coordinateMatch(closedList,right.x,right.y))) {
-						if(!(coordinateMatch(openList,right.x,right.y))) {
+					if(checkValidMove(map, right.x, right.y) &&! (coordinateMatch(closedList, right.x, right.y))) {
+						if(!(coordinateMatch(openList, right.x, right.y))) {
 							openList.add(right);
-						}else if(right.distanceTravelled>current.distanceTravelled+1) {
+						} else if(right.distanceTravelled > current.distanceTravelled + 1) {
 							right.parent = current;
-							right.distanceTravelled = current.distanceTravelled+1;
+							right.distanceTravelled = current.distanceTravelled + 1;
 						}
 					}
 				}
@@ -116,8 +135,8 @@ public class EnemySmart extends Enemy {
 					currentPath = currentPath.parent;
 				}
 			}while(!currentMatch(currentPath));
-		}else {
-//			System.out.println(" \n FAILED FINDING PATH");
+		} else {
+		//System.out.println(" \n FAILED FINDING PATH");
 	        Random rand = new Random(); 
 			int nextDirection = rand.nextInt(3);
 			boolean moved = false;
@@ -150,12 +169,17 @@ public class EnemySmart extends Enemy {
 				}
 			}			
 		}
-		if(path.size()>0) {
+		if(path.size() > 0) {
 			this.setPosition(path.get(0).x,path.get(0).y);
-		}else {
-//			System.out.println(" \n FAILED creating path");
+		} else {
+		//System.out.println(" \n FAILED creating path");
 		}
 	}
+	
+	/**
+ 	 * Overrides the method getEnemyName.
+ 	 * @return enemy:smart
+	 */
 	@Override
 	public String getEnemyName() {
 		return "enemy:smart";
