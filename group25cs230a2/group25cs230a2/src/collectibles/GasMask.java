0c104f3fef9e6f976b56c4a5a29afb99800d3fbd
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 19:05:49 +0000

    JavaDoc comments (Not sure, look at the others too)

--- group25cs230a2/group25cs230a2/src/collectibles/GasMask.java
+++ group25cs230a2/group25cs230a2/src/collectibles/GasMask.java
@@ -1,20 +1,35 @@
 package collectibles;
 
+/**
+ * A subclass that defines GasMask.
+ * @author Evans
+ * @version 1.2
+ */
+
 public class GasMask extends Collectible {
 
+	/**
+ 	 * Constructor method that spawns the gas mask in the specific coordinate.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+ 	 */
 	public GasMask(int x, int y) {
 		super(x, y, "gasMask.png");
 	}
 
+	/**
+ 	 * Spawns the gas mask in the origin (0,0).
+ 	 */
 	public GasMask() {
 		super("gasMask.png");
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+ 	 * Overrides the method getCollectibleName.
+ 	 * @return The collectible name: gasMask.
+ 	 */
 	@Override
 	public String getCollectibleName() {
-		// TODO Auto-generated method stub
 		return "gasMask";
 	}
-
 }
