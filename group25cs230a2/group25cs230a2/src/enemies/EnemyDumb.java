047b5b5d33b3afeb63eeb340514d10072f0fcfe1
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 07:08:57 +0000

    JavaDoc comments...

--- group25cs230a2/group25cs230a2/src/enemies/EnemyDumb.java
+++ group25cs230a2/group25cs230a2/src/enemies/EnemyDumb.java
@@ -1,12 +1,31 @@
 package enemies;
+
 import game.*;
 import java.lang.Math;
 
+/**
+ * A class that defines dumb enemy.
+ * @author Group25
+ * @version 1.0
+ */
+
 public class EnemyDumb extends Enemy {
+	
+	/**
+ 	 * Constructs dumb enemy in the specific coordinate.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+ 	 */
+	
 	public EnemyDumb(int x, int y) {
 		super(x, y);
 		setSprite("enemyDumb.png");
 	}
+	
+	/**
+ 	 * A method that decides the direction dumb enemy is moving.
+ 	 * @param map The map.
+ 	 */
 	
 	public void move (Map map) {
 		int px = map.getPlayer().getX();
@@ -16,7 +35,6 @@ public class EnemyDumb extends Enemy {
 		int distanceDown = map.distanceBetween(getX(), getY() + 1, px, py);
 		int distanceLeft = map.distanceBetween(getX() - 1, getY(), px, py);
 		int distanceRight = map.distanceBetween(getX() + 1, getY(), px, py);
-		
 		
 		int shortest = Math.min(Math.min(distanceUp, distanceDown), Math.min(distanceLeft, distanceRight));
 		
@@ -31,6 +49,11 @@ public class EnemyDumb extends Enemy {
 		}	
 	}
 
+	/**
+ 	 * Overrides the method getEnemyName.
+ 	 * @return enemy:dumb
+ 	 */
+	
 	@Override
 	public String getEnemyName() {
 		return "enemy:dumb";
