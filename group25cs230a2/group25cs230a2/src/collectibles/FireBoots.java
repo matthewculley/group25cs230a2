bda721c6271d11074676e94c26c42eacb8930a52
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 18:58:00 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/collectibles/FireBoots.java
+++ group25cs230a2/group25cs230a2/src/collectibles/FireBoots.java
@@ -1,18 +1,35 @@
 package collectibles;
 
+/**
+ * A subclass that defines FireBoots.
+ * @author Evans
+ * @version 1.1
+ */
+
 public class FireBoots extends Collectible {
 
+	/**
+ 	 * Constrcutor method that spawns the fire boots in the specific coordinate.
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+ 	 */
 	public FireBoots(int x, int y) {
 		super(x, y, "fireBoots.png");
 	}
 	
+	/**
+ 	 * Spawns the fire boots in the origin (0,0).
+ 	 */
 	public FireBoots() {
 		super(0, 0, "fireBoots.png");
 	}
 
+	/**
+ 	 * Get the collectible name.
+ 	 * @return The collectible name: fireBoots.
+ 	 */
 	@Override
 	public String getCollectibleName() {
 		return "fireBoots";
 	}
-
 }
