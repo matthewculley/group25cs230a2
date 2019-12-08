9a2f763543139895ccce236012ba2afcae700075
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 05:23:13 +0000

    JavaDoc comments

--- group25cs230a2/group25cs230a2/src/cells/Fire.java
+++ group25cs230a2/group25cs230a2/src/cells/Fire.java
@@ -1,9 +1,22 @@
 package cells;
+
+/**
+ * A class that defines Fire.
+ * @author Group25
+ * @version 1.1
+ */
+
 public class Fire extends Cell {
+	
 	public Fire() {
 		super("fire.png", true);
 	}
 
+	/**
+ 	 * Overrides the method getCellName.
+ 	 * @return The cell name: fire.
+ 	 */
+	
 	@Override
 	public String getCellName() {
 		return "fire";
