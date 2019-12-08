4a3c6f616dff4d1a2e6af34d7a99e5de05f073f4
alanH10 <57836751+alanH10@users.noreply.github.com>
2019-12-08 06:50:58 +0000

    JavaDoc comments (Parent?)

--- group25cs230a2/group25cs230a2/src/enemies/CoordinateCost.java
+++ group25cs230a2/group25cs230a2/src/enemies/CoordinateCost.java
@@ -1,6 +1,12 @@
 package enemies;
 import game.Map;
 
+/**
+ * This class defines the coordinate of the enemy.
+ * @author Group25
+ * version 1.0
+ */
+
 public class CoordinateCost {
 
 	protected int x;
@@ -13,6 +19,17 @@ public class CoordinateCost {
 	private int playerY;
 	protected CoordinateCost parent;
 	
+	/**
+ 	 * 
+ 	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param distanceTravelled Distance travelled of the enemy.
+	 * @param parent 
+	 * @param playerX The x coordinate of the player.
+	 * @param playerY The y coordinate of the player.
+	 * @param map The map.
+ 	 */
+	
 	public CoordinateCost(int x,int y,int distanceTravelled,CoordinateCost parent,int playerX, int playerY,Map map) {
 		this.x = x;
 		this.y = y;
@@ -24,6 +41,11 @@ public class CoordinateCost {
 		heuristic = map.distanceBetween(x, y, playerX, playerY);
 		totalCost = distanceTravelled+heuristic;
 	}
+	
+	/**
+ 	 * Checks the player is in a certain coordinate.
+ 	 * @return The result whether the coordinate have player or not.
+ 	 */
 	
 	public boolean isPlayer() {
 		if(x==playerX && y==playerY) {
