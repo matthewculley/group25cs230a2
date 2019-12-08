ac48cfbd1899149dae4edeb5d3e4d9939d4feb80
Ejones4321 <57711255+Ejones4321@users.noreply.github.com>
2019-12-08 21:42:12 +0000

    Removed redundancies

--- group25cs230a2/group25cs230a2/src/enemies/EnemyDumb.java
+++ group25cs230a2/group25cs230a2/src/enemies/EnemyDumb.java
@@ -5,7 +5,7 @@ import java.lang.Math;
 
 /**
  * A class that defines dumb enemy.
- * @author ethan J
+ * @author Ethan J
  * @version 1.3
  */
 
@@ -57,58 +57,3 @@ public class EnemyDumb extends Enemy {
 	}	
 		
 }
-	
-//	public void move() { //ADD CASE FOR EQUAL X/Y
-//		private Cell toUp = map.getAt(super.getX(),super.getY()-1) ;
-//		private Cell toDown = map.getAt(super.getX(),super.getY()+1);
-//		private Cell toLeft = map.getAt(super.getX()-1,super.getY()) ;
-//		private Cell toRight = map.getAt(super.getX()+1,super.getY());
-//		
-//		if(Math.abs(player.getY()-super.getY())>Math.abs(player.getX()-super.getX())) {
-//			if(player.getY()>super.getY()) {
-//				if(super.canTraverse(toDown)) {
-//					super.move('d');
-//				}else if(player.getX()>super.getX()) {
-//					if(super.canTraverse(toRight)) {
-//						super.move('r');
-//					}
-//				}else if(super.canTraverse(toLeft)) {
-//					super.move('l');
-//				}
-//			}else {
-//				if(super.canTraverse(toUp)) {
-//					super.move('u');
-//				}else if(player.getX()>super.getX()) {
-//					if(super.canTraverse(toRight)) {
-//						super.move('r');
-//					}
-//				}else if(super.canTraverse(toLeft)) {
-//					super.move('l');
-//				}
-//			}
-//		}else {
-//			if(player.getX()>super.getX()) {
-//				if(super.canTraverse(toRight)) {
-//					super.move('r');
-//				}else if(player.getY()>super.getY()) {
-//					if(super.canTraverse(toDown)) {
-//						super.move('d');
-//					}
-//				}else if(super.canTraverse(toUp)) {
-//					super.move('u');
-//				}
-//			}else {
-//				if(super.canTraverse(toLeft)) {
-//					super.move('l');
-//				}else if(player.getY()>super.getY()) {
-//					if(super.canTraverse(toDown)) {
-//						super.move('d');
-//					}
-//				}else if(super.canTraverse(toUp)) {
-//					super.move('u');
-//				}
-//			}
-//			
-//		}
-//	}
-//}
